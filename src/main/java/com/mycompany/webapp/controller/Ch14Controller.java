5e065733fd815750d8006d92d191fdd81a3a632d
chaeyeon4u <cy990426@naver.com>
2021-09-14 17:22:46 +0900

    210914: pager2

--- src/main/java/com/mycompany/webapp/controller/Ch14Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch14Controller.java
@@ -235,6 +235,7 @@ public class Ch14Controller {
 		}*/
 		int totalRows = boardService.getTotalBoardNum();
 		Pager pager = new Pager(10, 5, totalRows, pageNo);
+		model.addAttribute("pager", pager);
 		
 		List<Ch14Board> boards = boardService.getBoards(pager);
 		model.addAttribute("boards", boards);
